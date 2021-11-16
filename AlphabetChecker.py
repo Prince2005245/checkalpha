bb78e3e91eb9559df94ac108a6f64426536d9ffa
Luigi Boschetti <80232309+Prince2005245@users.noreply.github.com>
2021-11-16 20:12:24 +0000

    Added Functions

--- AlphabetChecker.py
+++ AlphabetChecker.py
@@ -1,6 +1,9 @@
-print("Whats up, its tyrones alphabet checker")
-character = input("Please enter in a single character: ")
-if((character >= "a" and character <= "z") or (character >= "A" and character <= "Z")):
-    print(character, "is in the Alphabet")
-else:
-    print(character, "is not in the Alphabet")
+def script():
+    print("Whats up, its tyrones alphabet checker")
+    character = input("Please enter in a single character: ")
+    if((character >= "a" and character <= "z") or (character >= "A" and character <= "Z")):
+        print(character, "is in the Alphabet")
+    else:
+        print(character, "is not in the Alphabet")
+if __name__ == "__main__":
+    script()
