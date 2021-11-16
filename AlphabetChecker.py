041cea1992fa01dfe0facd044f5433be601f123e
Luigi Boschetti <80232309+Prince2005245@users.noreply.github.com>
2021-11-16 20:10:52 +0000

    Add files via upload

---
+++ AlphabetChecker.py
@@ -0,0 +1,6 @@
+print("Whats up, its tyrones alphabet checker")
+character = input("Please enter in a single character: ")
+if((character >= "a" and character <= "z") or (character >= "A" and character <= "Z")):
+    print(character, "is in the Alphabet")
+else:
+    print(character, "is not in the Alphabet")
